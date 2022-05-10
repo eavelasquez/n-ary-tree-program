cbd8abac7eebf2ba6022e0d33d9b299c093f9097
Esteban Velasquez <eavc.30@gmail.com>
2022-05-09 20:49:57 -0500

    feat(back): add NAryTree interface

---
+++ src/narytreeprogram/NAryTree.java
@@ -0,0 +1,47 @@
+package narytreeprogram;
+
+/**
+ *  The {@code NAryTree} interface implements methods for n-ary tree.
+ *
+ *  @author ev
+ */
+public interface NAryTree {
+
+    public void print(Node temp);
+
+    public void insert(char data);
+
+    public void insert(Node temp, char parent, char data);
+
+    public void delete(Node temp, char data);
+
+    public void search(Node temp, char data);
+
+    public void printParents(Node temp);
+
+    public void printChildren(Node temp);
+
+    public void countLeaves(Node temp);
+
+    public void printTreeDegree(Node temp, char data);
+
+    public void printDegreeLeafByData(Node temp, char data);
+
+    public void printChildrenByData(Node temp, char data);
+
+    public void printSiblingsByData(Node temp, char data);
+
+    public void printLevelByData(Node temp, char data);
+
+    public void printTreeHeight(Node temp);
+
+    public void printParentByData(Node temp, char data);
+
+    public void printLeavesByLevel(Node temp, int level);
+
+    public boolean isLeaf(Node temp);
+
+    public boolean isEmpty(Node temp);
+
+    public String toString();
+}
