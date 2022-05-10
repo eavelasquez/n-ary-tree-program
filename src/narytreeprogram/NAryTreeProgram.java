6afe1493e756e4059bac9301de4cb306e70b77f8
Esteban Velasquez <eavc.30@gmail.com>
2022-05-09 19:22:22 -0500

    feat(back): update NAryTreeProgram file

--- src/narytreeprogram/NAryTreeProgram.java
+++ src/narytreeprogram/NAryTreeProgram.java
@@ -1,21 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/******************************************************************************
+ *  Compilation:  javac NAryTreeProgram.java
+ *  Execution:    java NAryTreeProgram
+ *
+ *  A program that creates a n-ary tree and prints it out.
+ *
+ ******************************************************************************/
 package narytreeprogram;
 
 /**
+ *  The {@code NAryTreeProgram} class represents a n-ary tree program.
  *
- * @author ev
+ *  @author ev
  */
 public class NAryTreeProgram {
 
     /**
-     * @param args the command line arguments
+     * Unit tests the {@code NAryTreeProgram} data type.
+     *
+     * @param args the command-line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World from N-Ary Tree Program!");
     }
-    
 }
