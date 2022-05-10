f5f9a106ca91490780ca00204dc1a7a79101fbb1
Esteban Velasquez <eavc.30@gmail.com>
2022-05-09 22:11:44 -0500

    feat(back): update NAryTreeProgram file
    - initial tests of the insert and printTree functions.

--- src/narytreeprogram/NAryTreeProgram.java
+++ src/narytreeprogram/NAryTreeProgram.java
@@ -21,5 +21,17 @@ public class NAryTreeProgram {
      */
     public static void main(String[] args) {
         System.out.println("Hello World from N-Ary Tree Program!");
+
+        NAryTreeGLL tree = new NAryTreeGLL();
+
+        tree.insert('a');
+        tree.insert(tree.getRoot(), 'a', 'b');
+        tree.insert(tree.getRoot(), 'a', 'c');
+        tree.insert(tree.getRoot(), 'a', 'd');
+        tree.insert(tree.getRoot(), 'b', 'e');
+        tree.insert(tree.getRoot(), 'b', 'f');
+        tree.insert(tree.getRoot(), 'c', 'g');
+
+        tree.printTree();
     }
 }
